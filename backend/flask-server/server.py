3f730d5ab2d081fb550b02643f9f0f740f3896d2
Christian Miller <christianmiller@christians-air.dhcp.nd.edu>
2023-04-01 13:52:18 -0400

    something is working

--- backend/flask-server/server.py
+++ backend/flask-server/server.py
@@ -8,21 +8,24 @@ CORS(app)
 # example of getting data from database for login
 @app.route('/login_info', methods=['POST']) 
 def login_info():
+    print('login_info')
     data = request.json
     username = data['username']
     password = data['password']
+    print(username, password)
 
-    conn = cx_Oracle.connect('FILL IN USERNAME/PASSWORD@HOST_INFO HERE')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM table_name WHERE username = :username AND password = :password', username=username, password=password)
-    result = cur.fetchall()
-    cur.close()
-    conn.close()
+    # conn = cx_Oracle.connect('FILL IN USERNAME/PASSWORD@HOST_INFO HERE')
+    # cur = conn.cursor()
+    # cur.execute('SELECT * FROM table_name WHERE username = :username AND password = :password', username=username, password=password)
+    # result = cur.fetchall()
+    # cur.close()
+    # conn.close()
+    response = jsonify({'success': True})
     
-    if result:
-        response = jsonify({'success': True})
-    else:
-        response = jsonify({'success': False})
+    # if result:
+    #     response = jsonify({'success': True})
+    # else:
+    #     response = jsonify({'success': False})
 
 
     return response
